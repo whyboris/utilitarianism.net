b6b85b006c1b6cacb0a86e86fd6d70ad62ff5d8c
Boris Yakubchik <yboris@yahoo.com>
2022-09-02 17:31:26 -0400

    auto-fix italic / underscores

--- README.md
+++ README.md
@@ -24,6 +24,16 @@ hugo -D
 
 All the pages were imported from Google Documents via [Docs to Markdown](https://workspace.google.com/u/0/marketplace/app/docs_to_markdown/700168918607) (see [documentation](https://github.com/evbacher/gd2md-html/wiki)).
 
+After adding a document it will likely need some manual fixes:
+- Update the metadata section (first few lines, see other files for reference)
+- It will likely have formatting errors with _italics_ (_underscores_), so open the `fixit.js` file, update the `FILE_TO_FIX` string, and execute: `node fixit.js`
+- Add the `{{< TOC >}}` to automatically generate the _Table of Contents_
+- Update the `    ` (four spaces - code block) to be `> ` (quote)
+- Update the "how to cite" section with shortcodes `{{< cite-start >}}` and `{{< cite-end >}}`
+- Add the _Action_ button shortcode `{{< button >}}`
+- If a footnote was multi-paragraph, add the indentation so the text stays with the footnote (rather than being rendered to the page at the bottom)
+- Look over the document for any other manual fixes
+
 ### Internationalization
 
 In the future we may want to use some `rtl` support
---
+++ fixit.js
@@ -0,0 +1,63 @@
+// Fix Italics Script
+// to fix formatting problems with underscores (italics)
+// 
+// Update this line:
+FILE_TO_FIX = "./content/types-of-utilitarianism.md";
+//
+// run `node fix.js` in your terminal
+//
+// DO NOT EDIT BELOW
+// =====================================================
+
+const fs = require("fs");
+
+fs.readFile(FILE_TO_FIX, "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR!");
+    console.log(err);
+    return;
+  }
+
+  const num_of_underlines = (data.match(new RegExp("_", "g")) || []).length;
+  console.log("Number of underlines:", num_of_underlines);
+
+  if (num_of_underlines % 2 == 0) {
+
+    const temp = data.split("_");
+
+    const finish = [];
+
+    let fixed = 0;
+
+    // The algorithm:
+    // if an even string in array ends with a space " " 
+    // then remove the space from the end 
+    // and prepend it to the next string in array
+    for (let i = 0; i < temp.length; i++) {
+      if (i % 2 != 0 && temp[i].endsWith(" ")) {
+        finish.push(temp[i].slice(0, -1));
+        temp[i + 1] = " " + temp[i + 1];
+        fixed = fixed + 1;
+      } else {
+        finish.push(temp[i]);
+      }
+    }
+
+    if (fixed > 0) {
+      console.log("Fixed", fixed, "incorrectly formatted italics!");
+
+      const final = finish.join("_");
+
+      fs.writeFile(FILE_TO_FIX, final, (err) => {
+        if (err) {
+          console.log("ERROR !!?!?!");
+          console.log(err);
+        }
+      });
+    } else {
+      console.log("No problems found with _italics_");
+    }
+  } else {
+    console.log("Not an even number of underscores '_' in the document!");
+  }
+});
