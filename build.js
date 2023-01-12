de1fd811db3010b67941a0406708e249ee721f94
Boris Yakubchik <yboris@yahoo.com>
2023-01-12 00:41:33 -0500

    build works

--- build.js
+++ build.js
@@ -1,74 +1,72 @@
-var fs = require('fs');
+var fs = require("fs");
 
 const flag = process.argv[2];
 
-const CSS_PATH = './assets/scss/lol.scss';
-
-if (!flag) {
-
-  console.log("this is a script to assist with building the website")
-  console.log("ERROR - no flag passed in");
-
-} else if (flag === "reset") {
-
-  const tomlString = getTomlString();
-
-  const newString = setAllFalse(tomlString);
-
-  writeTomlString(newString);
-
-  disablePDFSCSS()
-
-} else if (["search", "pdf", "production"].includes(flag)) {
-
-  const tomlString = getTomlString();
-  
-  const allFalse = setAllFalse(tomlString);
-  
-  const newString = allFalse.replace(flag + " = false", flag + " = true");
-  
-  writeTomlString(newString)
-
-} else {
-
-  console.log("ERROR - you can only pass in 'search', 'pdf', 'production', or 'reset'");
-
-}
+const CSS_PATH = "./assets/scss/lol.scss";
 
 if (flag === "pdf") {
   enablePDFSCSS();
 }
 
+if (!flag) {
+  console.log("This is a script to assist with the building of the website");
+  console.log("Please use `just` to build");
+} else if (flag === "pdfreset") {
+  disablePDFSCSS();
+} else if (flag === "reset") {
+  const tomlString = getTomlString();
+  const allFalse = setAllFalse(tomlString);
+  writeTomlString(allFalse);
+} else if (["search", "pdf", "production"].includes(flag)) {
+  const tomlString = getTomlString();
+  const allFalse = setAllFalse(tomlString);
+  const flagEnabled = allFalse.replace(flag + " = false", flag + " = true");
+  writeTomlString(flagEnabled);
+} else {
+  console.log(
+    "ERROR - you can only pass in 'search', 'pdf', 'pdfreset', 'production', or 'reset'"
+  );
+}
+
 function enablePDFSCSS() {
-  const scssFileString = fs.readFileSync(CSS_PATH, 'utf-8');
-  const pdfEnabled = scssFileString.replace("// @import 'pdf';","@import 'pdf';");
+  const scssFileString = fs.readFileSync(CSS_PATH, "utf-8");
+  const pdfEnabled = scssFileString.replace(
+    "\n// @import 'pdf';",
+    "\n@import 'pdf';"
+  );
   if (scssFileString !== pdfEnabled) {
-    fs.writeFileSync(CSS_PATH, pdfEnabled, 'utf-8');
+    fs.writeFileSync(CSS_PATH, pdfEnabled, "utf-8");
   }
 }
 
 function disablePDFSCSS() {
-  const scssFileString = fs.readFileSync(CSS_PATH, 'utf-8');
-  const pdfDisabled = scssFileString.replace("@import 'pdf';","// @import 'pdf';");
+  const scssFileString = fs.readFileSync(CSS_PATH, "utf-8");
+  const pdfDisabled = scssFileString.replace(
+    "\n@import 'pdf';",
+    "\n// @import 'pdf';"
+  );
   if (scssFileString !== pdfDisabled) {
-    fs.writeFileSync(CSS_PATH, pdfDisabled, 'utf-8');
+    fs.writeFileSync(CSS_PATH, pdfDisabled, "utf-8");
   }
 }
 
 function setAllFalse(incoming) {
   const pdfFalse = incoming.replace("pdf = true", "pdf = false");
   const searchFalse = pdfFalse.replace("search = true", "search = false");
-  const allFalse = searchFalse.replace("production = true", "production = false");
+  const allFalse = searchFalse.replace(
+    "production = true",
+    "production = false"
+  );
 
   return allFalse;
 }
 
 function getTomlString() {
-  const tomlString = fs.readFileSync('./config.toml', 'utf-8');
+  const tomlString = fs.readFileSync("./config.toml", "utf-8");
 
   return tomlString;
 }
 
 function writeTomlString(tomlString) {
-  fs.writeFileSync('./config.toml', tomlString, 'utf-8');
+  fs.writeFileSync("./config.toml", tomlString, "utf-8");
 }
--- justfile
+++ justfile
@@ -1,17 +1,26 @@
 run:
   hugo serve
 
+build: search pdf production
+
 @search:
-  node build.js search 
+  node build.js search
   hugo
   pagefind --source public
   echo ""
   echo "   🚀  Search index generated"
 
 @pdf:
-  node build.js pdf 
-  hugo serve & sleep 2 
-  cd pdf && npm start 
-  node build.js reset 
+  node build.js pdf
+  hugo serve & sleep 2
+  cd pdf && npm start
+  node build.js pdfreset
   echo ""
-  echo "   🚀  PDF generation finished" 
+  echo "   🚀  PDF generation finished"
+
+@production:
+  node build.js production
+  hugo
+  node build.js reset
+  echo ""
+  echo "   🚀  Production build finished"
